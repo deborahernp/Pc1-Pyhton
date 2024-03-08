0069e614db24e52a2bbb9e191ee529a508eafd1f
deborahernp <162033029+deborahernp@users.noreply.github.com>
2024-03-08 03:57:14 +0000

    Solución 3

---
+++ Pregunta 3.py
@@ -0,0 +1,11 @@
+# Número de payasos vendidos
+Payasos = int(input("Introduce el número de payasos vendidos: "))
+# Número de muñecas vendidas
+Muñecas = int(input("Introduce el número de muñecas vendidas: "))
+# Peso de cada payaso y muñeca en gramos
+Peso_payaso = 112
+Peso_muñeca = 75
+# Calcular el peso total del paquete
+Peso_total = (Peso_payaso * Payasos) + (Peso_muñeca * Muñecas)
+# Mostrar el peso total del paquete
+print(f"El peso total del paquete es {Peso_total} gramos.")
