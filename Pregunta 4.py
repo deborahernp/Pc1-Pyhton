e94d87e71dcd521cee518831ea2460cd5e8a9da3
deborahernp <162033029+deborahernp@users.noreply.github.com>
2024-03-08 03:58:02 +0000

    Solución 4

---
+++ Pregunta 4.py
@@ -0,0 +1,6 @@
+# Número entero positivo al usuario
+N = int(input("Introduce un número entero positivo (N): "))
+# Calcular la suma de los primeros N enteros positivos
+suma = (N * (N + 1)) // 2
+# Resultado de la suma
+print(f"La suma de los primeros {N} enteros positivos es: {suma}")
